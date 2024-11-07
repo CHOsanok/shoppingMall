6c136c0fab40b5414ed2306b1172bb85b054581d
ChoSangok <chod1510@naver.com>
2024-11-07 11:44:28 +0900

    test_api*

--- server/app.js
+++ server/app.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const indexRouter = require("./routes/index");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 const PORT = process.env.PORT || 4000;
 const allowedOrigins = [
   "http://localhost:3000",
   "https://shopping-mall-sigma.vercel.app",
 ];
-require("dotenv").config();
+
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+      // 모든 도메인 허용
+      callback(null, true);
     },
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    credentials: true, // 자격 증명 포함 요청 허용
   })
 );
 app.use(bodyParser.urlencoded({ extended: false }));
